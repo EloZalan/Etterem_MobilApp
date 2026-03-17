5c27f280735dd5c759b23f78f61d35a1b7eb3f89
Élő Zalán <90603993+EloZalan@users.noreply.github.com>
2026-03-17 18:11:49 +0100

    color update 1

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace WaiterApp;
+﻿using Microsoft.Maui.Controls;
+
+namespace WaiterApp;
 
 public partial class App : Application
 {
     public App(AppShell shell)
     {
         InitializeComponent();
+        UserAppTheme = AppTheme.Dark;
+
         MainPage = shell;
     }
 }
